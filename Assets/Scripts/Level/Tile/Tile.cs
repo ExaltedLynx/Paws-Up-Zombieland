23fb54becd51772244e06fccb270a3bd87d86332
ExaltedLynx <michael201103@gmail.com>
2023-09-26 12:38:33 -0400

    Implemented Basic Map Tiles
    Added prefabs for ground and elevated tile types as well as for the enemy spawn point and the player's base.

---
+++ Assets/Scripts/Level/Tile/Tile.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public enum TileType
+    {
+        Null = -1,
+        Ground = 0,
+        Elevated = 1,
+    }
+
+    [SerializeField] TileType type;
+    //add a field to store the unit on the tile once they are implemented
+    //private PlayableUnit unit;
+
+    public TileType GetTileType()
+    {
+        return type;
+    }
+
+    public void SetUnit()
+    {
+
+    }
+
+
+
+}
