2fb2f9574dc763571ae8ad25db8a2636c45aa5a0
SantiagoBoscan07 <santiagoboscan0702@gmail.com>
2023-10-06 14:45:49 -0400

    Enemy Pathfinding Commit
    Added functionality for the enemy to follow a path according to waypoints

---
+++ Assets/Scripts/Enemies/Waypoints.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+
+// Store the waypoints of the level
+public Transform[] waypoints;
+
+
+}
